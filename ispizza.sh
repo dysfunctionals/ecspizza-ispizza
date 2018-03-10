097a25f365bae23f5ec2464603c0e0ce6245c02b
Kajetan Champlewski <kchamplewski@gmail.com>
2018-03-10 15:03:00 +0000

    Modified ispizza.sh to use local classify_image.py script

--- ispizza.sh
+++ ispizza.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#MUST be run in a python3 environment, MUST have $IMAGESCRIPT set to the path to the classify_image.py script.
-#For more information on the script see https://www.tensorflow.org/tutorials/image_recognition
-eval python $IMAGESCRIPT --image_file $1 | grep pizza |awk '{sum+=$(NF)} END {printf "%.5f\n", sum}'
+#Depends on classify_image.py.
+python classify_image.py --image_file $1 | grep pizza |awk '{sum+=$(NF)} END {printf "%.5f\n", sum}'
