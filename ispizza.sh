0bfaefedfe18dd42ed5902befb957abc31ea4d9b
Kajetan Champlewski <kchamplewski@gmail.com>
2018-03-10 15:28:51 +0000

    Now stores model in relative directory instead of tmp.

--- ispizza.sh
+++ ispizza.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Depends on classify_image.py.
-python3 classify_image.py --image_file $1 | grep pizza |awk '{sum+=$(NF)} END {printf "%.5f\n", sum}'
+python3 classify_image.py --image_file $1 --model_dir ./model | grep pizza |awk '{sum+=$(NF)} END {printf "%.5f\n", sum}'
