b67abdbe132aee4c43a2908118da239fa668284d
Kajetan Champlewski <kchamplewski@gmail.com>
2018-03-11 12:16:05 +0000

    fixed bad import maybe hmm

--- verify_pizza.py
+++ verify_pizza.py
@@ -1,9 +1,8 @@
-set_image = __import__('verification.ecspizza-ispizza.classify_image').set_image
-classify = __import__('verification.ecspizza-ispizza.classify_image').classify
+classify_image = __import__('verification.ecspizza-ispizza.classify_image')
 
 def verify(imagePath):
-    set_image(imagePath)
-    result = classify()
+    classify_image.set_image(imagePath)
+    result = classify_image.classify()
     if("pizza" in result[0]):
         return u'🍕'
     else:
